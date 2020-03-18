f8a9fc7dc946a91f84c1bcab4f14a26e89e5d9a6
AdityaPNM98 <aditya.nambiar24@gmail.com>
2020-03-18 21:32:21 +0530

    Update sum.c
    Merge Test - 6:
    Expected op: Line4 merge conflict

--- sum.c
+++ sum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <conio.h> 
 // Fast-forward vs non-fast-forward: https://confluence.atlassian.com/bitbucket/git-fast-forwards-and-branch-management-329977726.html
-
+int add(); // Function declaration
 int main() {
 	printf("sum: ",a++); // Changed a+b to a++
 	return 0;
