d517e259d8d50d5c44e5378896bbbb94d0c7d882
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-03-18 21:32:43 +0530

    .

--- sum.c
+++ sum.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <conio.h> 
 // Fast-forward vs non-fast-forward: https://confluence.atlassian.com/bitbucket/git-fast-forwards-and-branch-management-329977726.html
-
 int main() {
 	printf("sum: ",a++); // Changed a+b to a++
 	return 0;
