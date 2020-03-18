36684e63a9b3299f9e9397c4e5b1c93baaf6d01d
AdityaPNM98 <aditya.nambiar24@gmail.com>
2020-03-18 20:22:15 +0530

    Update sum.c
    Merge Test - 3
    Changed line 3 on both, local & remote, repository resulting in two seperate timelines (diverged branches)
    Line3 is the same at both places.
    If git detects changes by 'content', then it will not generate merge conflict. Otherwise, it will generate merge conflict.

--- sum.c
+++ sum.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <conio.h> 
+// Fast-forward vs non-fast-forward: https://confluence.atlassian.com/bitbucket/git-fast-forwards-and-branch-management-329977726.html
 int main() {
 	printf("sum: ",a+b);
 	return 0;
